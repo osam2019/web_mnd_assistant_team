cb46bb75152197a38490fa16d1ac99b0af44c170
David Lee <leetdavidu@gmail.com>
2019-10-24 00:33:25 +0900

    fixed stuff being centered randomly

--- src/App.vue
+++ src/App.vue
@@ -25,20 +25,11 @@ body {
   font-size: 14pt;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.el-button.button-boing:hover {
+  -webkit-transform: translate(0, -2px);
+  -ms-transform: translate(0, -2px);
+  transform: translate(0, -2px);
 }
 </style>
